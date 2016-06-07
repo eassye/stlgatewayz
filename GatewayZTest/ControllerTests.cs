b8c4389e54e716971dd4cb05734d31a3ded55b51
eassye <eassye1@yahoo.com>
2016-06-07 01:50:11 -0500

    Created Unit Test Project and added a unit test class for Controller testing.

---
+++ GatewayZTest/ControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GatewayZ.Controllers.Web;
+
+namespace GatewayZTest
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
