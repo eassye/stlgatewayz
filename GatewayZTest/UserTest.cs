65e207d68390a1b0dfc0d6c769eb0b3c56dcb454
eassye <eassye1@yahoo.com>
2016-06-21 10:12:53 -0500

    added unit test project

---
+++ GatewayZTest/UserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GatewayZ;
+using GatewayZ.Models;
+
+namespace GatewayZTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            User user = new User();
+            user.displayName = "erick";
+
+            var expected = "erick";
+
+            Assert.AreEqual(user.displayName, expected);
+        }
+    }
+}
