c74043deb123a82743bcdb37cd3775ee5e197ad7
eassye <eassye1@yahoo.com>
2016-07-02 10:10:17 -0500

    Added userDAO and MemberService

---
+++ src/GatewayZ/Services/MembersServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GatewayZ.GatewayZDAO;
+
+namespace GatewayZ.Services
+{
+    public class MembersServices
+    {
+        public void gatherUsersByCompany()
+        {
+            UserDAO data = new UserDAO();
+
+            string club = "GathewayZ";
+
+            data.UsersList(club);
+        }
+    }
+}
