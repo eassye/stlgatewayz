e42ae6d0263b43fc04c3906cad6529047a2ecd91
eassye <eassye1@yahoo.com>
2016-07-02 13:02:27 -0500

    add dynamic member list from Mongo to members page

--- src/GatewayZ/Services/MembersServices.cs
+++ src/GatewayZ/Services/MembersServices.cs
@@ -12,7 +12,7 @@ namespace GatewayZ.Services
         {
             UserDAO data = new UserDAO();
 
-            string club = "GathewayZ";
+            string club = "GatewayZ";
 
             data.UsersList(club);
         }
