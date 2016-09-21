1a86ff4a85e07bcd8656c9752bea5a03a8f3a392
eassye <eassye1@yahoo.com>
2016-09-21 16:41:00 -0500

    refactored routing

--- src/GatewayZ/Startup.cs
+++ src/GatewayZ/Startup.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Framework.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Framework.Configuration;
+
 
 namespace GatewayZ
 {
     public class Startup
         {
-        public void ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {   
             services.AddMvc();
             //services.AddCaching();
@@ -15,18 +18,33 @@ namespace GatewayZ
                 options.IdleTimeout = TimeSpan.FromMinutes(30);
                 options.CookieName = ".Gatewayz";
             });
+
+            //var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+            //    .SetBasePath(env.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //    .AddEnvironmentVariables();
+            //Configuration = builder.Build();
         }
-            public void Configure(IApplicationBuilder app)
+
+        public void Configure(IApplicationBuilder app)
             {
             app.UseSession();
             app.UseStaticFiles();
             app.UseDeveloperExceptionPage();
 
-            app.UseMvc(config =>
+            //app.UseMvc(config =>
+            //{
+            //    config.MapRoute(name: "Default",
+            //                    template: "{controller}/{action}/{id?}",
+            //                    defaults: new { controller = "App", Action = "Index" });
+            //});
+
+            app.UseMvc(routes =>
             {
-                config.MapRoute(name: "Default",
-                                template: "{controller}/{action}/{id?}",
-                                defaults: new { controller = "App", Action = "Index" });
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=App}/{action=Index}/{id?}");
             });
         }
     }
